7ab74048b59887c01404584a6318dec4001229be
Jaid <6216144+Jaid@users.noreply.github.com>
2026-06-09 09:36:25 +0100

    Added more mixins
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import type {Dict} from 'more-types'
+import type {StringDict} from 'more-types'
 import type {Plugin} from 'vite'
 
 import {update} from 'es-toolkit/compat'
@@ -7,18 +7,44 @@ import flattenString from 'flatten-string'
 import makeMixins from '#src/lib/makeMixins.ts'
 
 type Options = {
-  additionalMixins?: Dict<string>
-  mixins?: Dict<string>
+  additionalMixins?: StringDict
+  defaultTheme?: 'dark' | 'light'
+  mixins?: StringDict
+  squareCategory?: 'landscape' | 'portrait'
+  tallHeight?: number
+  wideWidth?: number
 }
 
 const mediaMixinsPlugin = (options?: Options) => {
-  const defaultMixins = {
-    narrow: 'screen and (max-width: 599px)',
-    squat: 'screen and (max-height: 599px)',
+  const wideWidth = options?.wideWidth ?? 600
+  const tallHeight = options?.tallHeight ?? 600
+  const defaultTheme = options?.defaultTheme ?? 'dark'
+  const squareCategory = options?.squareCategory ?? 'portrait'
+  const defaultMixins: StringDict = {
+    narrow: `screen and not (min-width: ${wideWidth}px)`,
+    wide: `screen and (min-width: ${wideWidth}px)`,
+    squat: `screen and not (min-height: ${tallHeight}px)`,
+    tall: `screen and (min-height: ${tallHeight}px)`,
     static: '(prefers-reduced-motion: reduce)',
-    motion: 'not (prefers-reduced-motion: reduce)',
-    light: '(prefers-color-scheme: light)',
-    dark: '(prefers-color-scheme: dark)',
+    hover: '(hover: hover)',
+    aboveSrgb: '(color-gamut: p3) or (color-gamut: rec2020)',
+  }
+  defaultMixins.hoverless = `not (${defaultMixins.hover})`
+  defaultMixins.motion = `not (${defaultMixins.static})`
+  defaultMixins.srgb = `not (${defaultMixins.aboveSrgb})`
+  if (defaultTheme === 'light') {
+    defaultMixins.light = '(prefers-color-scheme: light)'
+    defaultMixins.dark = `not (${defaultMixins.light})`
+  } else {
+    defaultMixins.dark = '(prefers-color-scheme: dark)'
+    defaultMixins.light = `not (${defaultMixins.dark})`
+  }
+  if (squareCategory === 'portrait') {
+    defaultMixins.portrait = '(max-aspect-ratio: 1)'
+    defaultMixins.landscape = `not (${defaultMixins.portrait})`
+  } else {
+    defaultMixins.landscape = '(min-aspect-ratio: 1)'
+    defaultMixins.portrait = `not (${defaultMixins.landscape})`
   }
   const mixins = {
     ...options?.mixins || defaultMixins,
